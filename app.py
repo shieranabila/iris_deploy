6173c51fb50ccd0f85fe9157711866cac00f4421
shieranabila <shieranabila@students.unnes.ac.id>
2025-05-28 14:43:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title("Hello, World!")
 st.sidebar.header("dashboard")
+st.text("selamat datang!!")
 name = st.text_input("Masukkan nama Anda:")
 if name:
     st.write(f"Halo, {name}!")
