f767fc6cc7e91ce20ec23bd5152ed22f67157aa7
shieranabila <shieranabila@students.unnes.ac.id>
2025-05-28 14:36:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 st.title("Hello, World!")
 
-import streamlit as st
-
 st.set_page_config(page_title="dashboard")
 st.title("Selamat datang!")
 st.sidebar.header("dashboard")
+
+st.set_page_config(page_title="halaman 1")
+st.title("Hallo")
+st.sidebar.header("halaman 1")
+
 
 
