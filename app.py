5169444f1a15bc387aabf7fa66d3b55330540307
shieranabila <shieranabila@students.unnes.ac.id>
2025-05-28 14:16:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Hello, World!")
+
+st.set_page_config(page_tittle="dashboard")
+st.title("Selamat datang!")
+         st.sidebar.header("dashboard")
+
+st.set_page_config(page_tittle="halaman 1")
+st.title("Hello")
+         st.sidebar.header("halaman 1")
